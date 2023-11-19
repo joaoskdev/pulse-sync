b4b761edf845ea8337f14338fbce537ed42a7cbb
Joao Sabakeviski <130368135+joaoskdev@users.noreply.github.com>
2023-11-19 00:08:12 +0000

    Alteração na frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>"Eu amo muito a minha namorada Dora e faria tudo para ver ela feliz :) "</h1>
+    return <h1>"Eu amo muito a minha namorada Dora e faria tudo para ver ela feliz!"</h1>
 }
 
 export default Home;
